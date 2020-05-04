000e48cd4e6440fe0c13270434395d94310375e7
LokeshBabuTG <55309854+LokeshBabuTG@users.noreply.github.com>
2020-05-04 18:34:29 +0530

    1

--- herokutstapptgl/settings.py
+++ herokutstapptgl/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'herokutstapptgl.urls'
@@ -125,4 +126,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
